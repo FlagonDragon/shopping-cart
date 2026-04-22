ee415e1fad964efb73047e9ed450e9cf63ff3779
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-04-22 06:40:32 -0400

    add fakestoreapi call

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,41 @@
+import { useEffect } from "react";
+import { useState } from "react";
 // import classes from "../styles/Card.module.css";
 
+
 function Card() {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+
+    async function getProduct() {
+
+      try {        
+
+        const response = await fetch(`https://fakestoreapi.com/products/1`);
+
+        const productData = await response.json();
+
+        setData(productData);
+
+        console.log(productData);
+        
+        
+      } catch (error) {
+        console.error(error);
+      }
+
+    }
+
+    getProduct();
+
+  }, [])
 
     return(
         <div>
-            <div>Title</div>
+            <div>{data.title}</div>
             <div>Img</div>
-            <div>Info</div>
+            <div>Price: {data.price}</div>
         </div>
     )
 }
