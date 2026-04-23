f5794b4b10b82180ee7dcf82152d5bc5e97b7f37
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-04-23 04:54:43 -0400

    add prototype for product card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
-// import classes from "../styles/Card.module.css";
+import classes from "../styles/Card.module.css";
 
 
 function Card() {
@@ -32,10 +32,16 @@ function Card() {
   }, [])
 
     return(
-        <div>
-            <div>{data.title}</div>
-            <div>Img</div>
-            <div>Price: {data.price}</div>
+        <div className={classes.container}>
+
+            <div className={classes.imgDiv}>
+              <img className={classes.myImg} src={data.image}></img>
+            </div>
+
+            <div className={classes.dataDiv}>
+              <div>{data.title}</div>
+              <div>${data.price}</div>
+            </div>
         </div>
     )
 }
