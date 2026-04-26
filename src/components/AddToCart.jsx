67b8e0f75f7f38119322d217bda2be11142f8828
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-04-26 01:23:35 -0400

    add states to to AddToCart

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import classes from "../styles/AddToCart.module.css";
 
 function AddToCart() {
+    const [value, setValue] = useState("");
+    const [onCart, setOnCart] = useState(0);
+    
+    function handleClick() {
+        setOnCart(Number(onCart)+Number(value));
+        console.log(onCart);
+    }
 
     return(
         <div className={classes.container}>
-            <input type="number" min={0} max={99} className={classes.inputDiv}></input>
-            <button className={classes.btnDiv}>Add to Cart</button>
+            <input 
+                className={classes.inputDiv}
+                type="number" 
+                min={0} 
+                max={99}
+                value={value}
+                onChange={
+                    (e) => setValue(e.target.value)
+                }          
+            ></input>
+            <button 
+                className={classes.btnDiv}
+                onClick={handleClick}
+            >Add to Cart</button>
         </div>
     )
 }
