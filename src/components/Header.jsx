d99df68b024b34af216e406da432050e60fb7fd2
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-05-05 04:34:23 -0400

    update header styling and DONE DONE DONE!!!!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,19 @@ function Header({ inCart }) {
     
     return(
         <div className={classes.container}>
-            <nav><div>
+            <nav><div className={classes.leftDiv}>
                 <Link to="/store/home">Home</Link>
             </div></nav>
             <nav><div className={classes.middleDiv}>
                 <Link to="/store/shop">Shop</Link>
             </div></nav>
-            <nav><div>
-                <Link to="/store/cart">Cart ({inCart})</Link>
+            <nav><div className={classes.rightDiv}>
+                <Link to="/store/cart">
+                <div className={classes.innerRight}>
+                    <div>Cart</div> 
+                    <div className={classes.inCart}>{inCart}</div>
+                </div>
+                </Link>
             </div></nav>
         </div>
     )
